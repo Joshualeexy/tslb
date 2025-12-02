3786e52b4bc5a45f817b7ef0aad89c459f886e9b
Joshualeexy <oghenekevwejoshualee@gmail.com>
2025-12-02 05:19:56 +0100

    Add TLS configuration options for Reverb server

--- config/reverb.php
+++ config/reverb.php
@@ -34,8 +34,15 @@ return [
             'path' => env('REVERB_SERVER_PATH', ''),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => [
+                    'local_cert' => env('REVERB_TLS_CERT', 'C:/Users/oghen/.vite-plugin-mkcert/cert.pem'),
+                    'local_pk' => env('REVERB_TLS_KEY', 'C:/Users/oghen/.vite-plugin-mkcert/dev.pem'),
+                    'verify_peer' => false,
+                    'passphrase' => null,
+
+                ],
             ],
+
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
                 'enabled' => env('REVERB_SCALING_ENABLED', false),
